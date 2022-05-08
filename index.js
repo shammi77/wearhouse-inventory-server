77b17da8f0215595da410bcef22d9a0aa7112ab9
shammi77 <nusratshammi.vutu@gmail.com>
2022-05-08 16:33:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+require('dotenv').config()
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// user: user1db
+// password: ZSIYKzXoo0deQvjP
+
+app.get('/', (req, res) =>{
+    res.send('wearhouse running');
+});
+
+app.listen(port, () =>{
+    console.log('Listening to port' , port);
+})
