8199b4405ec0bbb84443655b827f3794bf80faed
shammi77 <nusratshammi.vutu@gmail.com>
2022-05-09 21:18:00 +0600

    final push

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
 app.use(cors());
@@ -10,6 +11,31 @@ app.use(express.json());
 
 // user: user1db
 // password: ZSIYKzXoo0deQvjP
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ikqdf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const inventoryCollection = client.db('wearHouse').collection('inventory');
+
+        app.get('/inventory', async (req, res) =>{
+            const query={};
+            const cursor = inventoryCollection.find(query);
+            const inventories = await cursor.toArray();
+            res.send(inventories);
+        })
+
+    }
+    finally{
+
+    }
+}
+run().catch(console,dir);
+
 
 app.get('/', (req, res) =>{
     res.send('wearhouse running');
