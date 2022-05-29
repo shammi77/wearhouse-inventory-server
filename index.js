a929e1c20e76ca67ee11e20a6f02ccf7a05d54fe
shammi77 <nusratshammi.vutu@gmail.com>
2022-05-29 22:03:35 +0600

    final push

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 5000;
 require('dotenv').config();
-
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
+
 app.use(cors());
 app.use(express.json());
 
-// user: user1db
-// password: ZSIYKzXoo0deQvjP
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fcfn2.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri);
 
+async function run (){
+    try{
+        await client.connect();
+        const itemCollection = client.db('inventory').collection('items');
 
+        app.get('/item', async(req,res)=>{
+            const query={};
+            const cursor = itemCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        });
+    }
+    finally{
 
+    }
+}
+run().catch(console.dir);
 
-
-app.get('/', (req, res) =>{
-    res.send('wearhouse running');
+app.get('/', async (req, res) => {
+    res.json('Running inventory server');
 });
 
-app.listen(port, () =>{
-    console.log('Listening to port' , port);
+app.listen(port, ()=>{
+    console.log('Listening to port',port);
 })
